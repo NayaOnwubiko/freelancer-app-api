8fefbf8416a785e80db1f543a9acfb16b167e428
NayaOnwubiko <naya.onwubiko@gmail.com>
2023-06-06 11:44:36 -0400

    created upstream repo

---
+++ controllers/user.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
+
+export const deleteUser = async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+
+        if(req.userId !== user._id.toString()){
+           return (createError(403,"You can only delete your account"));
+        }
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).send("Successfully deleted");
+    
+};
+
+export const getUser = async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+
+    res.status(200).send(user);
+};
